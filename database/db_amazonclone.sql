3b5dd26c508c17fa2e05d362e24e324c74fd7ae0
Caique <carlosneto726@gmail.com>
2023-05-18 15:50:33 -0300

    atualizando a imagem bugada

--- database/db_amazonclone.sql
+++ database/db_amazonclone.sql
@@ -104,7 +104,7 @@ INSERT INTO tb_produtos(id_loja, titulo,descricao,estrelas,marca,preco,categoria
 (7, 'Macarrão Spaghetti Barilla','Spaguetti 500g Barilla serve 5 pessoas',4.0,'Barilla', 9.59,'Cozinha', 20000,'0','img/produtos/cozinha_macarrao.jpg'),
 (3, 'Google Chromecast 3','Transmita seu conteúdo de onde e quando quiser | Streaming em Full HD', 5.0,'Google', 219,'Tecnoligia',15000,'0','img/produtos/tech_chromecast.jpg'),
 (1, 'Echo Dot (4ª Geração)','Smart Speaker com Alexa | Música, informação e Casa Inteligente - Cor Preta',5.0,'Amazon', 269, 'Tecnoligia', 4562,'0','img/produtos/tech_echodot.jpg'),
-(1, 'Suporte de Notebook','Suporte Uptable OCTOO, Preto', 5.0,'Amazon', 39.99,'Acessorios',15000,'0','img/produtos/tech_suportenotebook.jpg'),
+(1, 'Suporte de Notebook','Suporte Uptable OCTOO, Preto', 5.0,'Amazon', 39.99,'Acessorios',15000,'0','img/produtos/acessorios_suportenotebook.jpg'),
 (1, 'Capa Gel Banco', 'Capa Gel Banco Selim Almofada Para Bicicleta Bike Preto', 5.0,'Amazon', 42.93,'Acessorios',15000,'0','img/produtos/acessorios_capa_bike.jpg'),
 (1, 'Apoio De Leitura Livro E Tablet - Oliver','Apoio De Leitura Livro E Tablet - Oliver', 5.0,'Amazon', 79.90,'Acessorios',15000,'0','img/produtos/acessorios_suporte_livro.jpg'),
 (1, 'Carregador Universal Ultra Rápido','Geonav Carregador Universal Ultra Rápido, Power Delivery 25W, 1 Porta USB-C, CH25PDWT, Branco', 5.0,'Amazon', 122.32,'Acessorios',15000,'0','img/produtos/acessoriso_carregador_universal.jpg'),
