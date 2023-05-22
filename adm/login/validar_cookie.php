b33496525912320329cb706498b19273ccb17b63
Caique <carlosneto726@gmail.com>
2023-05-22 13:18:21 -0300

    documentando o validar_cookie

--- adm/login/validar_cookie.php
+++ adm/login/validar_cookie.php
@@ -5,19 +5,27 @@
 
     $manipula = new manipular_dados();
 
+    // Recebendo as variaveis do POST
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    // Definindo a tebela da consulta do banco de dados
     $manipula->setTable("tb_usuarios");
+
+    // Retorna a quantidade de usuarios registrados com o email e usuarios passados como parametro
     $linhas = $manipula->validarLogin($email, $password);
-    
+
+    // Caso não exista um usuário registrado no banco de dados
     if($linhas == 0){
         $_SESSION['jsAlert'] = "<script>alert('Usuário e/ou senha incorreto(s)')</script>";
         header("Location: ../../index.php");
         exit();
     }else{
+        // Definindo a variavel global e o coockie  email
         $_SESSION['email'] = $email;
         setcookie("email", $email, time() + (86400 * 30), "/");  
+
+        // Voltando para o index
         header("Location: ../../index.php");
         exit();
     }
