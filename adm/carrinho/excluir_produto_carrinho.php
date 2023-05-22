c9ef9713220a9d2f8314578c67322fb0ecd4cca8
Caique <carlosneto726@gmail.com>
2023-05-22 13:12:44 -0300

    documentando o excluir_produto_carrinho

--- adm/carrinho/excluir_produto_carrinho.php
+++ adm/carrinho/excluir_produto_carrinho.php
@@ -4,12 +4,13 @@ include_once("../../dao/manipular_dados.php");
 
 $manipula = new manipular_dados();
 
+// Pegando as informações do POST
 $id_produto = $_GET['id'];
 
-echo $id_produto;
-
+// Deletando o produto do carrinho
 $manipula->dellProdutoCarrinho($id_produto);
 
+// Voltando para a pagina do carrinho
 header("Location: ../../?secao=carrinho");
 exit();
 
