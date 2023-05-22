d25ef5c36783def6c9afea2ac2bef19b6a3ca8dd
Caique <carlosneto726@gmail.com>
2023-05-22 13:11:22 -0300

    documentando o adicionar_carrinho

--- adm/carrinho/adicionar_carrinho.php
+++ adm/carrinho/adicionar_carrinho.php
@@ -4,16 +4,18 @@ include_once("../../dao/manipular_dados.php");
 
 $manipula = new manipular_dados();
 
+// Recebendo as variaveis do POST
 $id_produto = $_POST['id_produto'];
 $id_usuario = $manipula->getIdByEmail($_SESSION['email'])[0]['id'];
 $qtd = $_POST['qtd'];
 
-
+// Inserindo o produto no carrinho
 $manipula->setTable("tb_carrinho");
 $manipula->setFields("id_usuario,id_produto,qtd");
 $manipula->setDados("'$id_usuario','$id_produto','$qtd'");
 $manipula->insert();
 
+// Voltando para  apagina do carrinho
 header("Location: ../../?secao=carrinho");
 exit();
 
