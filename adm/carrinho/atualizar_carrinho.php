4b789413d0b235e6d64bfc0f4a2ff25cc6f8a9bf
Caique <carlosneto726@gmail.com>
2023-05-28 00:36:25 -0300

    implementando um novo arquivo que atualiza a quantidade de produto no carrinho

---
+++ adm/carrinho/atualizar_carrinho.php
@@ -0,0 +1,15 @@
+
+<?php
+
+include_once("../../dao/manipular_dados.php");
+
+$manipula = new manipular_dados();
+
+// Atualizando no banco da dados a nova quantidade no carrinho
+$manipula->updateProdutoCarrinho($_POST["id"], $_POST["new_qtd"]);
+
+// Voltando para  apagina do carrinho
+header("Location: ../../?secao=carrinho");
+exit();
+
+?>
